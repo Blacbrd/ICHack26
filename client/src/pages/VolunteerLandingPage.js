be9cfc37561a5e3423a827518859914198baafe2
aqibfaruqui <aqibfaruqui@gmail.com>
2026-02-01 01:38:09 +0000

    antigravity time

--- client/src/pages/VolunteerLandingPage.js
+++ client/src/pages/VolunteerLandingPage.js
@@ -245,7 +245,7 @@ const VolunteerLandingPage = ({ user }) => {
             </div>
             <button 
               className="btn-settings"
-              onClick={() => navigate('/settings')} /* Navigate to settings page */
+              onClick={() => navigate('/profile')} /* Navigate to settings page */
               title="Settings"
             >
               <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
